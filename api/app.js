60c397e6e014c5b5e726d07b146f9b4768271832
HugoMichard <hugomichard@yahoo.com>
2022-03-20 12:52:40 +0100

    deploy heroku 4

--- api/app.js
+++ api/app.js
@@ -51,11 +51,6 @@ app.use('/api/platforms', platformsRouter);
 app.use('/api/wires', wiresRouter);
 app.use('/api/dexs', dexsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -75,4 +70,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'))
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 module.exports = app;
