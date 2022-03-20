4d9bba61414d01c94a8ea47ff95582a9f2bbfee4
HugoMichard <hugomichard@yahoo.com>
2022-03-20 12:27:06 +0100

    heroku test deploy 2

--- api/app.js
+++ api/app.js
@@ -68,11 +68,11 @@ app.use(function(err, req, res, next) {
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '/client/public')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/public/index.html'))
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
 });
 
 module.exports = app;
